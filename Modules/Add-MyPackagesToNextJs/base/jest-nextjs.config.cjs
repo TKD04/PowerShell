e27aa578c84529af2632ad1d6f9ce14d052952c9
TKD04 <tkd04fordev@outlook.com>
2024-12-02 23:02:49 +0900

    Fix properties order according to `eslint-plugin-perfectionist`

--- Modules/Add-MyPackagesToNextJs/base/jest-nextjs.config.cjs
+++ Modules/Add-MyPackagesToNextJs/base/jest-nextjs.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   coverageProvider: "v8",
-  roots: ["<rootDir>"],
-  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
-  testEnvironment: "jsdom",
   preset: "ts-jest",
+  roots: ["<rootDir>"],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
 };
