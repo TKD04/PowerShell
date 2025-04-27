bf035a1d872def1b2525cc165fcb45a320dc563d
TKD04 <tkd04fordev@outlook.com>
2025-04-27 16:12:17 +0900

    Add `Remove-MyNpmScript`

---
+++ Modules/Remove-MyNpmScript/Remove-MyNpmScript.psm1
@@ -0,0 +1,30 @@
+﻿<#
+.SYNOPSIS
+Removes the given name of npm script from package.json.
+
+.PARAMETER ScriptName
+#>
+function Remove-MyNpmScript {
+    [OutputType([void])]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$ScriptName
+    )
+
+    [string]$packagePath = '.\package.json'
+
+    if (!(Test-MyStrictPath -LiteralPath $packagePath)) {
+        throw '.\package.json was not found.'
+    }
+
+    [hashtable]$package = Import-MyJSON -LiteralPath $packagePath -AsHashTable
+    [boolean]$hasScript = $package.scripts.ContainsKey($ScriptName)
+
+    if ($hasScript) {
+        $package.scripts.Remove($ScriptName)
+    }
+    else {
+        throw 'The key "{0}" was not found in npm scripts.' -f $ScriptName
+    }
+    Export-MyJSON -LiteralPakh $packagePath -CustomObject $package
+}
