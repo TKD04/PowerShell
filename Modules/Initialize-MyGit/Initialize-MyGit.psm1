03517c7603cc048cdf8f8c749f28a3c989b72e29
TKD04 <tkd04fordev@outlook.com>
2025-05-04 14:36:37 +0900

    Add parameters `$UseNode` and `$UsePython`

--- Modules/Initialize-MyGit/Initialize-MyGit.psm1
+++ Modules/Initialize-MyGit/Initialize-MyGit.psm1
@@ -1,20 +1,42 @@
 ﻿<#
 .SYNOPSIS
 Initializes git in the current directory.
+
+.PARAMETER UseNode
+Whether to adds .gitignore for Node.js.
+
+.PARAMETER UsePython
+Whether to adds .gitignore for Python.
 #>
 function Initialize-MyGit {
     [Alias('ginit')]
     [OutputType([void])]
-    param()
+    param(
+        [switch]$UseNode,
+        [switch]$UsePython
+    )
 
     if (Test-MyStrictPath -LiteralPath '.\.git') {
         throw 'Git repository is already in place.'
     }
     git init $args
     if (!(Test-MyStrictPath -LiteralPath '.\.gitignore')) {
-        New-Item -Path '.\' -Name '.gitignore' -ItemType 'file'
+        if ($UseNode) {
+            Join-Path -Path $PSScriptRoot -ChildPath '\common\Node.gitignore' |
+            Copy-Item -Destination '.\.gitignore'
+        }
+        elseif ($UsePython) {
+            Join-Path -Path $PSScriptRoot -ChildPath '\common\Python.gitignore' |
+            Copy-Item -Destination '.\.gitignore'
+        }
+        else {
+            New-Item -Path '.\' -Name '.gitignore' -ItemType 'file'
+            git add '.\.gitignore'
+        }
     }
-    git add '.\.gitignore'
+    else {
+        Write-Warning -Message '.gitignore is already in place.'
+    }
     git commit -m 'First commit'
     Write-MySuccess -Message 'Created Git repository in the current directory.'
 }
