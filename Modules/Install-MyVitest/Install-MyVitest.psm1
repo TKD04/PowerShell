cf493a91cb502b09dafa734c8adb1a15ed879673
TKD04 <tkd04fordev@outlook.com>
2025-05-21 14:34:00 +0900

    Add `Install-MyVitest`

---
+++ Modules/Install-MyVitest/Install-MyVitest.psm1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+Adds Vitest to the current directory.
+#>
+function Install-MyVitest {
+    [OutputType([void])]
+    param()
+
+    [string[]]$devDependencies = @(
+        '@vitest/coverage-v8'
+        'vitest'
+    )
+
+    Add-MyNpmScript -NameToScript @{
+        'test'     = 'vitest'
+        'coverage' = 'vitest run --coverage'
+    }
+    pnpm add -D @devDependencies
+    git add '.\package.json' '.\pnpm-lock.yaml'
+    git commit -m 'Add Vitest'
+}
