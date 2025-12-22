1f3b6ae311b6633e6e6c01b37507c0f0233da424
TKD04 <155821058+TKD04@users.noreply.github.com>
2025-12-22 15:35:55 +0900

    Move functions from individual .psm1 files to .ps1 and consolidate under MyDevTools.psm1

---
+++ Modules/MyDevTools/MyDevTools.psm1
@@ -0,0 +1,10 @@
+﻿if (!(Test-Path -LiteralPath "$PSScriptRoot\Functions")) {
+    throw 'Functions directory could not be found.'
+}
+
+# Loads all the functions under the modules by dot-source
+Get-ChildItem -LiteralPath "$PSScriptRoot\Functions" -File -Filter '*.ps1' -Recurse |
+Select-Object -Property FullName |
+ForEach-Object -ThrottleLimit 5 -Parallel {
+    . $_
+}
