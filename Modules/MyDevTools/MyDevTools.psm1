524e9f663a141b0e45bd53ba1ea036fc9b999cb2
TKD04 <155821058+TKD04@users.noreply.github.com>
2025-12-22 20:33:57 +0900

    Remove non-working feature to load functions with parallel execution for PS7+

--- Modules/MyDevTools/MyDevTools.psm1
+++ Modules/MyDevTools/MyDevTools.psm1
@@ -3,18 +3,9 @@
 }
 
 # Loads all the functions under the modules by dot-source
-if ($PSVersionTable.PSVersion -ge [Version]'7.0.0') {
-    Get-ChildItem -LiteralPath "$PSScriptRoot\Functions" -File -Filter '*.ps1' -Recurse |
-    Select-Object -Property FullName |
-    ForEach-Object -ThrottleLimit 5 -Parallel {
-        . $_
-    }
-}
-else {
-    $functionFiles = Get-ChildItem -LiteralPath "$PSScriptRoot\Functions" -File -Filter '*.ps1' -Recurse |
-    Select-Object -Property FullName
+$functionFiles = Get-ChildItem -LiteralPath "$PSScriptRoot\Functions" -File -Filter '*.ps1' -Recurse |
+Select-Object -Property FullName
 
-    foreach ($functionFile in $functionFiles) {
-        . $functionFile
-    }
+foreach ($functionFile in $functionFiles) {
+    . $functionFile
 }
