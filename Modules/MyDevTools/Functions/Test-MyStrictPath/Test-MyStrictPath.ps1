583f7b960b6874aaa017fdff45a931ce96e706f5
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-01-11 19:25:20 +0900

    docs: remove redundant blank lines

--- Modules/MyDevTools/Functions/Test-MyStrictPath/Test-MyStrictPath.ps1
+++ Modules/MyDevTools/Functions/Test-MyStrictPath/Test-MyStrictPath.ps1
@@ -18,10 +18,8 @@ For these reasons, this script was created:
 
 - Test-Path in Windows PowerShell returns an error instead of $false
 if a path is an empty string (i.e. '').
-
 - Test-Path in Windows PowerShell returns $true instead of $false
 if a path is a blank string (e.g. ' ').
-
 - Test-Path in Windows PowerShell and PowerShell returns $true instead of $false
 if a path is a valid one and has trailing whitespace (e.g. '/path/to/file ').
 #>
