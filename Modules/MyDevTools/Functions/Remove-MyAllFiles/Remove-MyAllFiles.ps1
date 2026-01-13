3d21f2d3c95c058d6a88d3030bf977e94be8b95d
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-01-13 20:38:09 +0900

    refactor: add CmdletBiding with SupoprtsShouldProcess

--- Modules/MyDevTools/Functions/Remove-MyAllFiles/Remove-MyAllFiles.ps1
+++ Modules/MyDevTools/Functions/Remove-MyAllFiles/Remove-MyAllFiles.ps1
@@ -3,10 +3,13 @@
 Remove all files, not directories, from the current directory.
 #>
 function Remove-MyAllFiles {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     [OutputType([void])]
     param ()
 
-    Get-ChildItem -File | Remove-Item -Confirm
+    if ($PSCmdlet.ShouldProcess("$PWD\*.*", 'Remove all the files in the directory')) {
+        Get-ChildItem -File | Remove-Item
+    }
 }
 
 Set-Alias -Name 'rmfiles' -Value 'Remove-MyAllFiles'
