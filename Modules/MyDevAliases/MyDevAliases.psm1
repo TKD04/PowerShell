cf825a52ffda31c0ce924b122d9f689463b60e8f
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-01-30 18:44:33 +0900

    fix: function loading failure caused by script block scope

--- Modules/MyDevAliases/MyDevAliases.psm1
+++ Modules/MyDevAliases/MyDevAliases.psm1
@@ -1,16 +1,14 @@
 ﻿Set-StrictMode -Version 3.0
 $ErrorActionPreference = 'Stop'
 
-& {
-    [string]$scriptDir = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
-    [string[]]$scriptFiles = @()
+[string]$scriptDir = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
+[string[]]$scriptFiles = @()
 
-    if (-not (Test-Path -LiteralPath $scriptDir -PathType 'Container')) {
-        throw "Functions directory could not be found: $scriptDir"
-    }
+if (-not (Test-Path -LiteralPath $scriptDir -PathType 'Container')) {
+    throw "Functions directory could not be found: $scriptDir"
+}
 
-    $scriptFiles = (Get-ChildItem -LiteralPath $scriptDir -File -Filter '*.ps1').FullName
-    foreach ($file in $scriptFiles) {
-        . $file
-    }
+$scriptFiles = (Get-ChildItem -LiteralPath $scriptDir -File -Filter '*.ps1').FullName
+foreach ($file in $scriptFiles) {
+    . $file
 }
