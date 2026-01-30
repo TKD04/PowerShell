da6b42b13699b0b3c88b46a6e21bee3a836370b2
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-01-30 11:44:51 +0900

    refactor: check PowerShell edition and version in `$PROFILE`

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,11 @@
 ﻿Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+
+if ($PSVersionTable.PSEdition -ne 'Core' -or $PSVersionTable.PSVersion.Major -lt 7) {
+    [string]$currentEdition = $PSVersionTable.PSEdition
+    [string]$currentVersion = $PSVersionTable.PSVersion
+
+    Write-Error -Message "Unsupported PowerShell environment (Edition: $currentEdition, Version: $currentVersion)."
+    Write-Warning -Message 'This profile and associated scripts require PowerShell 7+ (Core edition).'
+    throw 'Environment specification mismatch.'
+}
