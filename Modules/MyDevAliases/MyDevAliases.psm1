a5894e0542c3f35cdb96e230eb9570034121f0c3
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-02-26 12:28:57 +0900

    refactor: avoid declaring empty variables beforehand

--- Modules/MyDevAliases/MyDevAliases.psm1
+++ Modules/MyDevAliases/MyDevAliases.psm1
@@ -2,13 +2,12 @@
 $ErrorActionPreference = 'Stop'
 
 [string]$scriptDir = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
-[string[]]$scriptFiles = @()
 
 if (-not (Test-Path -LiteralPath $scriptDir -PathType 'Container')) {
     throw "Functions directory could not be found: $scriptDir"
 }
 
-$scriptFiles = (Get-ChildItem -LiteralPath $scriptDir -File -Filter '*.ps1').FullName
+[string[]]$scriptFiles = (Get-ChildItem -LiteralPath $scriptDir -File -Filter '*.ps1').FullName
 foreach ($file in $scriptFiles) {
     . $file
 }
--- Modules/MyDevTools/MyDevTools.psm1
+++ Modules/MyDevTools/MyDevTools.psm1
@@ -2,13 +2,12 @@
 $ErrorActionPreference = 'Stop'
 
 [string]$scriptDir = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
-[string[]]$scriptFiles = @()
 
 if (-not (Test-Path -LiteralPath $scriptDir -PathType 'Container')) {
     throw "Functions directory could not be found: $scriptDir"
 }
 
-$scriptFiles = (Get-ChildItem -LiteralPath $scriptDir -File -Filter '*.ps1' -Recurse).FullName
+[string[]]$scriptFiles = (Get-ChildItem -LiteralPath $scriptDir -File -Filter '*.ps1' -Recurse).FullName
 foreach ($file in $scriptFiles) {
     . $file
 }
