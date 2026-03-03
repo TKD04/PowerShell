8999a94caf940a51cc3c5aae6769c98b9bebcc87
TKD04 <155821058+TKD04@users.noreply.github.com>
2026-03-03 17:31:44 +0900

    refactor: remove redundant variables

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,5 @@
 $ErrorActionPreference = 'Stop'
 
 if ($PSVersionTable.PSEdition -ne 'Core' -or $PSVersionTable.PSVersion.Major -lt 7) {
-    [string]$currentEdition = $PSVersionTable.PSEdition
-    [string]$currentVersion = $PSVersionTable.PSVersion
-
-    throw "Unsupported PowerShell environment (Edition: $currentEdition, Version: $currentVersion). Requires PowerShell 7+ (Core)."
+    throw "Unsupported PowerShell environment (Edition: $($PSVersionTable.PSEdition), Version: $($PSVersionTable.PSVersion)). Requires PowerShell 7+ (Core)."
 }
